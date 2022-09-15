320035d02ac1f4cdf5a12696ee65edeb09eb5005
abhishekgarfield <abhishek2759@gmail.com>
2022-09-15 11:33:23 +0530

    making navcard

--- components/NavigationCard.js
+++ components/NavigationCard.js
@@ -1,13 +1,104 @@
 import { useNavigation } from "@react-navigation/native";
-import { View ,Text} from "react-native";
-const NavigationCard=()=>{
-    const navigation=useNavigation();
-    return(
-        <View style={{backgroundColor:"red"}}>
-<Text onPress={()=>{
-    navigation.navigate("rideoptionscard")
-}}>navigationcard</Text>
-        </View>
-    );
-}
+import { View, Text, TextInput } from "react-native";
+import { useState } from "react";
+import { setDestination } from "../features/navslice";
+const NavigationCard = () => {
+  const dispatch = useD;
+  const navigation = useNavigation();
+  const [text, setText] = useState("");
+  const [data, setData] = useState([]);
+  return (
+    <View>
+      <View
+        style={{
+          backgroundColor: "rgb(247, 245, 245)",
+          borderRadius: 5,
+          marginVertical: 20,
+          alignItems: "center",
+          flexDirection: "row",
+        }}
+      >
+        <TextInput
+          onChangeText={(newText) => {
+            setText(newText);
+
+            const url = `https://api.geoapify.com/v1/geocode/autocomplete?text=${text}&apiKey=${AUTOCOMPLETE_MAPS_APIKEY}`;
+            if (text.length >= 0) {
+              fetch(url, { method: "GET" })
+                .then((response) => {
+                  return response.json();
+                })
+                .then((data) => {
+                  setData(data);
+                  data.features.map(({ properties }) => {
+                    console.log(properties.name);
+                  });
+                });
+            }
+          }}
+          defaultValue={text}
+          style={{
+            flexGrow: 1,
+            fontSize: 20,
+            backgroundColor: "rgb(247, 245, 245)",
+            padding: 8,
+          }}
+          placeholder="Where to go ?"
+        />
+        {text?.length > 0 && (
+          <Icon
+            name="close"
+            type="antdesign"
+            color="white"
+            size={10}
+            onPress={() => {
+              setData({});
+              setText("");
+              dispatch(setDestination(null));
+            }}
+            style={{
+              backgroundColor: "grey",
+              borderRadius: 50,
+              padding: 5,
+              marginVertical: 5,
+            }}
+          />
+        )}
+      </View>
+      {data.features?.map(({ properties }, index) => {
+        if (properties.name) {
+          return (
+            <TouchableOpacity
+              onPress={() => {
+                setText(
+                  `${properties.name},${properties.state},${properties.country}`
+                );
+                dispatch(
+                  setDestination({
+                    location: {
+                      long: properties.lon,
+                      lat: properties.lat,
+                    },
+                    description: `${properties.name},${properties.state},${properties.country}`,
+                  })
+                );
+                setData({});
+              }}
+              key={index}
+              style={{
+                borderBottomColor: "lightgrey",
+                marginBottom: 10,
+                borderBottomWidth: 1,
+              }}
+            >
+              <Text
+                style={{ fontWeight: 15, fontWeight: "700", padding: 7 }}
+              >{`${properties.name},${properties.state},${properties.country}`}</Text>
+            </TouchableOpacity>
+          );
+        }
+      })}
+    </View>
+  );
+};
 export default NavigationCard;
--- screens/mapscreen.js
+++ screens/mapscreen.js
@@ -12,12 +12,13 @@ import RideOptionsCrad from "../components/RideOptionsCard"
 const Mapscreen = () => {
   const Stack=createStackNavigator();
   const origin = useSelector((state) => state.navigation.origin);
+  const destination=useSelector(state=>state.navigation.destination);
   useEffect(() => {
     console.log(origin.description);
   }, []);
   return (
     <View style={{ flexDirection: "column", display: "flex", flexGrow: 1 }}>
-      <Map origin={origin}/>
+      <Map origin={origin} destination={destination}/>
       <View style={{ backgroundColor: "yellow", flexGrow: 1 }}>
         <Stack.Navigator>
           <Stack.Screen name="navigationcard" component={NavigationCard}
