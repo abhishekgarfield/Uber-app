7abfca2381ca7256fff5b4b998fc4650d4b4b493
abhishekgarfield <abhishek2759@gmail.com>
2022-09-16 12:36:02 +0530

    implemented prici.ng

--- components/map.js
+++ components/map.js
@@ -21,7 +21,7 @@ const Map = ({ origin, destination }) => {
       .then((data) => {
         dispatch(settraveTimeinformation({
           time:(data.features[0].properties.time/60),
-          distance:data.features[0].properties.distance/1000
+          distance:data.features[0].properties.distance/1000+"km"
         }))
         setCoorddata(data);
       });
