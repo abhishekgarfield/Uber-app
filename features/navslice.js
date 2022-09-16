dd454af6fca1a9352cfaa99a2a4d5cdb84966026
abhishekgarfield <abhishek2759@gmail.com>
2022-09-16 13:35:04 +0530

    feature clode cleaned

--- features/navslice.js
+++ features/navslice.js
@@ -1,37 +1,35 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-
-const initialState={
-    origin:null,
-    destination:null,
-    travelTimeinformation:null,
+const initialState = {
+  origin: null,
+  destination: null,
+  travelTimeinformation: null,
 };
 
-export const navslice=createSlice({
-    name:"navigation",
-    initialState,
-    reducers:{
-                setOrigin:(state,action)=>{
-                    state.origin=action.payload;
-                },
-                setDestination(state,action)
-                {
-                    state.destination=action.payload;
-                },
-                settraveTimeinformation:function (state,action){
-                    if(action.payload.time>60)
-                    {
-                     action.payload.time=(action.payload.time/60).toFixed(1)+" hours ";
-                     state.travelTimeinformation=action.payload
-                    }
-                    else{
-                        action.payload.time=action.payload.time.toFixed(1)+" min "
-                        state.travelTimeinformation=action.payload;
-                    }
-                }
-                
-    }
-})
-export const {setOrigin,setDestination,settraveTimeinformation}=navslice.actions;
- const checkOrigin=(state)=>{state.navigation.origin}
+export const navslice = createSlice({
+  name: "navigation",
+  initialState,
+  reducers: {
+    setOrigin: (state, action) => {
+      state.origin = action.payload;
+    },
+    setDestination(state, action) {
+      state.destination = action.payload;
+    },
+    settraveTimeinformation: function (state, action) {
+      if (action.payload.time > 60) {
+        action.payload.time = (action.payload.time / 60).toFixed(1) + " hours ";
+        state.travelTimeinformation = action.payload;
+      } else {
+        action.payload.time = action.payload.time.toFixed(1) + " min ";
+        state.travelTimeinformation = action.payload;
+      }
+    },
+  },
+});
+export const { setOrigin, setDestination, settraveTimeinformation } =
+  navslice.actions;
+const checkOrigin = (state) => {
+  state.navigation.origin;
+};
 export default navslice.reducer;
